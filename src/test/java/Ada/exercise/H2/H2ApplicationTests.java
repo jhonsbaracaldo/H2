8c57a18bc17c53cf4eda139607b804597b18b877
jhonsbaracaldo <jhonsbaracaldo@gmail.com>
2023-12-05 20:19:30 -0500

    Probando base H2

---
+++ src/test/java/Ada/exercise/H2/H2ApplicationTests.java
@@ -0,0 +1,13 @@
+package Ada.exercise.H2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
